8af23cdd8d9cf6181de1e8c30ae896ec5e48dc2b
Jay Aher <jaysureshaher@gmail.com>
2023-11-22 04:00:41 +0530

    Day - 1 Done

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int odd = 0, even = 0;
+    while (n > 0)
+    {
+        int temp = n % 10;
+        if (temp % 2 == 0)
+        {
+            even += temp;
+        }
+        else
+        {
+            odd += temp;
+        }
+        n /= 10;
+    }
+    cout << even << " " << odd << endl;
+}
